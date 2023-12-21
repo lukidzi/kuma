33ff588c1b4320df87102f9594c41fe1518988ba
Lukasz Dziedziak <lukidzi@gmail.com>
2023-12-21 11:18:12 +0100

    te
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- pkg/plugins/runtime/gateway/generator.go
+++ pkg/plugins/runtime/gateway/generator.go
@@ -10,6 +10,7 @@ import (
 	"github.com/pkg/errors"
 
 	mesh_proto "github.com/kumahq/kuma/api/mesh/v1alpha1"
+	"github.com/kumahq/kuma/pkg/core/permissions"
 	core_mesh "github.com/kumahq/kuma/pkg/core/resources/apis/mesh"
 	"github.com/kumahq/kuma/pkg/core/resources/model"
 	core_xds "github.com/kumahq/kuma/pkg/core/xds"
@@ -143,6 +144,11 @@ func gatewayListenerInfoFromProxy(
 	externalServices := meshCtx.Resources.ExternalServices()
 
 	var listenerInfos []GatewayListenerInfo
+
+	matchedExternalServices := permissions.MatchExternalServicesTrafficPermissions(
+		proxy.Dataplane, externalServices, meshCtx.Resources.TrafficPermissions(),
+	)
+
 	outboundEndpoints := core_xds.EndpointMap{}
 	for k, v := range meshCtx.EndpointMap {
 		outboundEndpoints[k] = v
@@ -151,7 +157,7 @@ func gatewayListenerInfoFromProxy(
 	esEndpoints := xds_topology.BuildExternalServicesEndpointMap(
 		ctx,
 		meshCtx.Resource,
-		externalServices.Items,
+		matchedExternalServices,
 		meshCtx.DataSourceLoader,
 		proxy.Zone,
 	)
